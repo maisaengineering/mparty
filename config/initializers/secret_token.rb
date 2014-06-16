bcf9d26abd7449ae67083b41ab1bd42514c36d3a
maisaengineering <contact@maisasolutions.com>
2014-06-16 12:13:26 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Invitation::Application.config.secret_key_base = '7d9e7ace71ccbda5a591225fcec31a0f3531f3f3149be1e9f5f3020c22800742229580b81a1f018bfcf9bfe589c0397dc933c9739a2b15bb842a76badd0ce80f'
