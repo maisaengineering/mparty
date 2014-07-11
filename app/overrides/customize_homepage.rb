90013c3c55e06fc56fde15abbec141ebf92a9edb
maisaengineering <contact@maisasolutions.com>
2014-07-11 12:37:34 +0530

    Fixing home page issue.

--- app/overrides/customize_homepage.rb
+++ app/overrides/customize_homepage.rb
@@ -8,12 +8,13 @@ Deface::Override.new(
 	:virtual_path =>"spree/shared/_nav_bar",
 	:name => "remove_search-bar",
 	:remove => "li#search-bar")
+
 Deface::Override.new(
 	:virtual_path =>"spree/shared/_main_nav_bar",
 	:name => "remove_main-nav-bar",
 	:remove => "ul#main-nav-bar")
+
 Deface::Override.new(:virtual_path  => "spree/home/index",
-	:replace => "[data-hook='homepage_products']",
-	:name => "homepage_events",
-	:partial => "spree/home/homepage_events")
+	:remove => "[data-hook='homepage_products']",
+	:name => "homepage_products")
 	
