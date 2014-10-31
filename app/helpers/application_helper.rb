75e9050de3af4efb0f68274cd35f5599ee71ff43
maisaengineering <contact@maisasolutions.com>
2014-10-31 16:57:36 +0530

    custom flash messages added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,30 @@ module ApplicationHelper
 			false
 		end	
   end
+
+  def bootstrap_class_for(flash_type)
+    # case flash_type
+    #   when "success"
+    #     "alert-success"   # Green
+    #   when "error"
+    #     "alert-danger"    # Red
+    #   when "alert"
+    #     "alert-warning"   # Yellow
+    #   when "notice"
+    #     "alert-info"      # Blue
+    #   else
+    #     flash_type.to_s
+    # end
+
+
+    {
+        success: 'alert-success',# Green
+        error: 'alert-danger',# Red
+        alert: 'alert-warning',# Yellow
+        notice: 'alert-info' # Blue
+    }[flash_type.to_sym] || flash_type.to_s
+
+  end
+
+
 end
