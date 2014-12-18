ef4821f45990acf0984f5800b549cc68ee498189
maisaengineering <contact@maisasolutions.com>
2014-12-18 10:32:07 +0530

    Removing venue admin ability from csr&adding venues tab in adin section.

--- app/models/csr_ability.rb
+++ app/models/csr_ability.rb
@@ -27,7 +27,6 @@ class CsrAbility
       can :manage, Spree::PaymentMethod
       can :manage, Spree::InventoryUnit
       can :manage, Spree::OptionValue
-      can :manage, Venue
     end
   end
 end
