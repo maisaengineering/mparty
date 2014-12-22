901df50a8ab81c92f6e0e5179615321b53ba4d47
maisaengineering <contact@maisasolutions.com>
2014-12-22 18:23:22 +0530

    Inviter wishlist page

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,37 +3,38 @@ class InvitesController < ApplicationController
   skip_before_filter :auth_user
   #layout 'spree_application'
 
-  def update_invitaion
+  def update_invitation
     @invitaion = Invite.where(token: params[:token]).first
     @invitaion.update_attributes(joined: params[:status])
     @event = @invitaion.event
-    session[:event_id] = @event.id
-    session[:invitaion_id] = @invitaion.id
-    if signed_in?
-      if @invitaion.recipient_email == current_spree_user.email
-        #redirect_to show_invitation_path(@event.id)
-        #TODO change has_wishlist in invitation table if user adds wishlist later
-        if @invitaion.has_wishlist == true
-          @wishlist = @event.wishlist
-          flash[:notice] = "Your friend likes following gifts."
-          render "/events/_wishlist_cart"#, layout: 'application'
-        else
-          flash[:error] = "Invitation doesn't have wishlist"
-          redirect_to event_path(id: @event.id)
-        end
-      else
-        flash[:error] = "Access denied.You are not invited to this event"
-        redirect_to event_path(id: @event.id)
-      end  
-    else  
-      if @invitaion.has_wishlist == true 
-        @wishlist = @event.wishlist
-        flash[:notice] = "Your friend likes following gifts."
-        render "/events/_wishlist_cart"#, layout: 'application'
-      else  
-        redirect_to event_path(id: @event.id)
-      end 
-    end 
+    @wishlist = @event.wishlist
+    # session[:event_id] = @event.id
+    # session[:invitaion_id] = @invitaion.id
+    # if signed_in?
+    #   if @invitaion.recipient_email == current_spree_user.email
+    #     #redirect_to show_invitation_path(@event.id)
+    #     #TODO change has_wishlist in invitation table if user adds wishlist later
+    #     if @invitaion.has_wishlist == true
+    #       @wishlist = @event.wishlist
+    #       flash[:notice] = "Your friend likes following gifts."
+    #       render "/events/_wishlist_cart"#, layout: 'application'
+    #     else
+    #       flash[:error] = "Invitation doesn't have wishlist"
+    #       redirect_to event_path(id: @event.id)
+    #     end
+    #   else
+    #     flash[:error] = "Access denied.You are not invited to this event"
+    #    # redirect_to event_path(id: @event.id)
+    #   end
+    # else
+    #   if @invitaion.has_wishlist == true
+    #     @wishlist = @event.wishlist
+    #     flash[:notice] = "Your friend likes following gifts."
+    #     render "/events/_wishlist_cart"#, layout: 'application'
+    #   else
+    #     redirect_to event_path(id: @event.id)
+    #   end
+    # end
   end
 
 =begin  private
