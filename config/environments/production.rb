a320b5355d4a6dfc58778f345053f406d68a7728
maisaengineering <contact@maisasolutions.com>
2014-12-23 15:14:08 +0530

    smtp disabled in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,20 +84,20 @@ Invitation::Application.configure do
   ENV['SENDER'] = "maisa.engineers@gmail.com"
 
 # ActionMailer Config
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.smtp_settings = {
-      :enable_starttls_auto => true,
-      :address => "smtpout.secureserver.net",
-      :port => 80,
-      :domain => "maisasolutions.com",
-      :authentication => :plain,
-      :user_name => 'labs@maisasolutions.com',
-      :password => 'MPRIDE786',
-  }
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.raise_delivery_errors = false
+#   config.action_mailer.default :charset => "utf-8"
+#
+#   config.action_mailer.smtp_settings = {
+#       :enable_starttls_auto => true,
+#       :address => "smtpout.secureserver.net",
+#       :port => 80,
+#       :domain => "maisasolutions.com",
+#       :authentication => :plain,
+#       :user_name => 'labs@maisasolutions.com',
+#       :password => 'MPRIDE786',
+#   }
 
 
 end
