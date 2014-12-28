5eee0c3214ccd0257673793cb583631f50a31a11
maisaengineering <contact@maisasolutions.com>
2014-12-28 22:09:31 +0530

    Selected date available... on Event new

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -22,4 +22,15 @@ class VenuesController < ApplicationController
     @contact = @venue.venue_contacts.first
   end
 
+  def check_availability
+    @event = Event.new(session[:event_data])
+    @venue = Venue.find(params[:id])
+    booked_venues_dates =  @venue.venue_calendars.where('start_date >= ? AND end_date <= ?', @event.starts_at,@event.starts_at).exists?
+    if !(booked_venues_dates)
+      @available = true      
+    else      
+      @available = false
+    end
+    render layout: false
+  end
 end
