b9ffe4876112433d9f6bb6de3605d5eee8a60b4d
maisaengineering <contact@maisasolutions.com>
2014-12-29 15:28:09 +0530

    Carrierwave Env specific storage

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,20 +7,17 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    dir = "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    Rails.env.production? ? dir : "uploads/#{dir}"
   end
 
    def fog_directory
      if Rails.env.production?
        ENV['IS_HEROKU'].eql?('yes') ? 'test_mparty_pictures786' : 'mparty_pictures786'
-     else
-       'test_mparty_pictures786'
      end
    end
 
