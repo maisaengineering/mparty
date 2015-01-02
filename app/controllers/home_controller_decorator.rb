794d7425987516f42a2966be33d4576d75337d00
maisaengineering <contact@maisasolutions.com>
2015-01-02 15:36:04 +0530

    Public Events search by name

--- app/controllers/home_controller_decorator.rb
+++ app/controllers/home_controller_decorator.rb
@@ -2,7 +2,12 @@ Spree::HomeController.class_eval do
   skip_before_filter :auth_user
 
   def index
-    @trending_events = Event.trending.page(params[:page]).per(4)
+    @trending_events = Event.trending
+    if params[:query].present?
+      @trending_events = @trending_events.where(Event.arel_table[:name].matches("%#{params[:query]}%"))
+      flash.now[:notice] = "Total #{ActionController::Base.helpers.pluralize(@trending_events.count,'result')} results found for your search"
+    end
+    @trending_events = @trending_events.page(params[:page]).per(8)
     render layout: false if request.xhr?
   end
 
