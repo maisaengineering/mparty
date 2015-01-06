26c2edfe4f8270f1266fe72f7bafe10582c4a07e
maisaengineering <contact@maisasolutions.com>
2015-01-06 08:05:07 +0530

    Invitee wishlist cart update/remove

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,26 +1,33 @@
 Spree::OrdersController.class_eval do
-	
-	#Overriding populate action to add multiple items to a cart at a time
-	# Adds a new item to the order (creating a new order if none already exists)
-    def populate
-      populator = Spree::OrderPopulator.new(current_order(create_order_if_necessary: true), current_currency)
 
-      flag = false
-
-      params[:variant_id].each_with_index do |item,i|
-      	if params[:quantity]["#{i}"].to_i > 0
-      		populator.populate(params[:variant_id]["#{i}"].to_i, params[:quantity]["#{i}"].to_i)
-      		flag = true
-      	end	
-      end	
-
-      if flag
-        respond_with(@order) do |format|
-          format.html { redirect_to cart_path }
-        end
-      else
-        flash[:error] = 'Please select quantity'  #populator.errors.full_messages.join(" ")
-        redirect_to :back
+  #Overriding populate action to add multiple items to a cart at a time
+  # Adds a new item to the order (creating a new order if none already exists)
+  def populate
+    populator = Spree::OrderPopulator.new(current_order(create_order_if_necessary: true), current_currency)
+    @flag = false
+    params[:variant_id].each_with_index do |item,i|
+      if params[:quantity]["#{i}"].to_i > 0
+        populator.populate(params[:variant_id]["#{i}"].to_i, params[:quantity]["#{i}"].to_i)
+        @flag = true
       end
     end
+
+    if @flag
+      flash[:success] = 'Items added to cart please checkout'
+    else
+      flash.now[:error] = 'Please select quantity'  #populator.errors.full_messages.join(" ")
+    end
+
+    respond_with(@order) do |format|
+      format.html {
+        if @flag
+          redirect_to cart_path
+        else
+          redirect_to :back
+        end
+      }
+      format.js{}
+    end
+
+  end
 end
