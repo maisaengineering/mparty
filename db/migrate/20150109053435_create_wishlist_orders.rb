b1f1cdd65659ad8b4c3d7c8bc213319fec3638cf
maisaengineering <contact@maisasolutions.com>
2015-01-09 13:17:21 +0530

    Modeling for wishlist orders ,associated to events after purchase

---
+++ db/migrate/20150109053435_create_wishlist_orders.rb
@@ -0,0 +1,14 @@
+class CreateWishlistOrders < ActiveRecord::Migration
+  def change
+    create_table :wishlist_orders do |t| 
+      t.integer :order_id, null: false
+      t.integer :wishlist_id, null: false
+      t.integer :wished_product_id, null: false
+      t.integer :purchased_quantity,null: false,default: 0
+      t.timestamps
+    end
+    add_index :wishlist_orders, :order_id
+    add_index :wishlist_orders, :wishlist_id
+    add_index :wishlist_orders, :wished_product_id
+  end
+end
