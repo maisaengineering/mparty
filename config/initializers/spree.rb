7e5365616dde609abf74746cbb044d4b43019962
maisaengineering <contact@maisasolutions.com>
2015-02-02 16:27:03 +0530

    Adding google fog directory to spree products.

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -48,3 +48,28 @@ Spree::ToFriendMailer.class_eval do
     mail(opts)
   end
 end	
+
+if Rails.env.production?
+
+attachment_config = {
+  :storage => :fog,
+  :fog_credentials => "#{Rails.root}/config/gce.yml",
+  :fog_directory => ENV['IS_HEROKU'].eql?('yes') ? 'test_mparty_products' : 'mparty_products', 
+
+  styles: {
+      mini:     "48x48>",
+      small:    "100x100>",
+      product:  "240x240>",
+      large:    "600x600>"
+  },
+
+  path:           "/spree/:class/:id/:style/:basename.:extension",
+  default_url:    "/spree/:class/:id/:style/:basename.:extension",
+  default_style:  "product"
+}
+
+attachment_config.each do |key, value|
+  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+end
+
+end
