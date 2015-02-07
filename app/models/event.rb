eee71b721afc9a221ac562e1615812b9775bc757
maisaengineering <contact@maisasolutions.com>
2015-02-07 13:05:41 +0530

    event name in alphabetical orders

--- app/models/event.rb
+++ app/models/event.rb
@@ -41,7 +41,7 @@ class Event < ActiveRecord::Base
   scope :past, -> { where("starts_at < ?", Date.today)}
   #scope :upcoming, -> { where("starts_at >= ?", Date.today).order(starts_at: :asc)}
   #scope :upcoming, -> { where("start_time >=? AND starts_at =? OR starts_at >?",Time.now+5.5.hour,Date.today,Date.today).order(starts_at: :asc,start_time: :asc)}
-  scope :upcoming, -> { where("end_time >=? AND ends_at =? OR ends_at >? ",Time.now+5.5.hour,Date.today,Date.today).order(starts_at: :asc,start_time: :asc)}
+  scope :upcoming, -> { where("end_time >=? AND ends_at =? OR ends_at >? ",Time.now+5.5.hour,Date.today,Date.today).order(starts_at: :asc,start_time: :asc,name: :asc)}
   scope :invited, ->(email) { where(id: Invite.where(recipient_email: email).map(&:event_id))}
 
   scope :trending,-> {  public.upcoming.order(starts_at: :asc) }
