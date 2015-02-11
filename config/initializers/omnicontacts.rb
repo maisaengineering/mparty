8632b78fb776575cc1fdc58d872cd08e3198ddca
maisaengineering <contact@maisasolutions.com>
2015-02-11 17:50:37 +0530

    sign_up facebook authentication fail

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -5,3 +5,9 @@ Rails.application.middleware.use OmniContacts::Builder do
 
 	importer :gmail, gmail_app_id, secret, {:redirect_path => "/contacts/gmail/contact_callback"}	
 end
+OmniAuth.config.on_failure = Proc.new do |env|
+	env['devise.mapping'] = Devise.mappings[:spree_user]
+	controller_name  = ActiveSupport::Inflector.camelize(env['devise.mapping'].controllers[:omniauth_callbacks])
+	controller_klass = ActiveSupport::Inflector.constantize("#{controller_name}Controller")
+	controller_klass.action(:failure).call(env)
+end
