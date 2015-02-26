7e4dce8e2dcbed77dc677aef2b8c3250917a40a9
imaginationcoder <labs@maisasolutions.com>
2015-02-26 20:33:38 +0530

    CSR role 1

--- app/models/csr_ability.rb
+++ app/models/csr_ability.rb
@@ -3,35 +3,35 @@ class CsrAbility
 
   def initialize user
     if user.has_spree_role?(:csr)
-      can :manage, Spree::Order
-      can :manage, Spree::Product
-      can :manage, Spree::User do |resource|
-        resource.has_spree_role?(:user)
-      end
-      cannot :destroy, Spree::User
-
-      can :manage, Spree::Shipment
-      cannot :update, Spree::Shipment
-      cannot :resend, Spree::Order
-      can :manage, Spree::Shipment::ManifestItem
-      cannot :split, Spree::Shipment::ManifestItem
-      can :manage, Spree::ShippingMethod
-      can :manage, Spree::ShippingRate
-      can :manage, Spree::Address
-      can :manage, Spree::StateChange
-      can :manage, Spree::Payment
-      can :manage, Spree::LineItem
-      can :manage, Spree::ReturnAuthorization
-      can :manage, Spree::Adjustment
-      can :manage, Spree::Variant
-      can :manage, Spree::PaymentMethod
-      can :manage, Spree::InventoryUnit
-      can :manage, Spree::OptionValue
-      can [:manage], Venue
-      can [:show, :edit, :update, :destroy], Venue do |resource|
-        resource.user_id == user.id
-      end  
-      can [:admin, :manage], Spree::Admin::VenuesController  
+      can [:admin,:manage], Spree::Order
+      # can :manage, Spree::Product
+      # can :manage, Spree::User do |resource|
+      #   resource.has_spree_role?(:user)
+      # end
+      # cannot :destroy, Spree::User
+      #
+      # can :manage, Spree::Shipment
+      # cannot :update, Spree::Shipment
+      # cannot :resend, Spree::Order
+      # can :manage, Spree::Shipment::ManifestItem
+      # cannot :split, Spree::Shipment::ManifestItem
+      # can :manage, Spree::ShippingMethod
+      # can :manage, Spree::ShippingRate
+      # can :manage, Spree::Address
+      # can :manage, Spree::StateChange
+      # can :manage, Spree::Payment
+      # can :manage, Spree::LineItem
+      # can :manage, Spree::ReturnAuthorization
+      # can :manage, Spree::Adjustment
+      # can :manage, Spree::Variant
+      # can :manage, Spree::PaymentMethod
+      # can :manage, Spree::InventoryUnit
+      # can :manage, Spree::OptionValue
+      # can [:manage], Venue
+      # can [:show, :edit, :update, :destroy], Venue do |resource|
+      #   resource.user_id == user.id
+      # end
+      can [:admin, :manage], Venue
     end
   end
 end
