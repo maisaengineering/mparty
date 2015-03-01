eac92d6b9ba220cef351b425febc8557568e6b02
imaginationcoder <labs@maisasolutions.com>
2015-03-01 17:25:00 +0530

    imgkit for heroku path

--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,7 +1,7 @@
-# IMGKit.configure do |config|
-#   #if Rails.env.production?
-#     config.wkhtmltoimage =   Rails.root.join('lib', 'wkhtmltoimage-amd64').to_s
-#   # else
-#   #   config.wkhtmltoimage = '/usr/local/bin/wkhtmltoimage'
-#   # end
-# end
+IMGKit.configure do |config|
+  if Rails.env.production?
+    config.wkhtmltoimage =   Rails.root.join('lib', 'wkhtmltoimage-amd64').to_s
+  else
+    #config.wkhtmltoimage = '/usr/local/bin/wkhtmltoimage'
+  end
+end
